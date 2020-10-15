9b8600ec2e387b4292ea41662f86d8d4330d13d6
Ryan Brown <piston47@gmail.com>
2020-10-15 13:51:22 +1100

    Added placement logic to Robot

--- src/domain/robot.ts
+++ src/domain/robot.ts
@@ -1,20 +1,66 @@
-import { RobotDirection, left as directionLeft, right as directionRight } from "./direction"
-export default class Robot {
-    readonly x: number;
-    readonly y: number;
-    readonly direction: RobotDirection;
+import { left as directionLeft, right as directionRight, RobotDirection } from "./direction"
 
-    public constructor(x : number, y : number, direction : RobotDirection) {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
+export default class Robot {
+    readonly maxX: number;
+    readonly maxY: number;
+    private _x?: number;
+    private _y?: number;
+    private _direction?: RobotDirection;
+
+    public constructor(maxX : number, maxY : number) {
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+
+    get x() : number {
+        return this._x;
+    }
+
+    get y() : number {
+        return this._y;
+    }
+
+    get direction() : RobotDirection {
+        return this._direction;
+    }
+
+    place(x : number, y : number, direction : RobotDirection) : Robot {
+        if(this.isValid(x, y ,direction)) {
+            return this.copy(x, y, direction)
+        }
+        // Invalid, ignore
+        return this;
     }
 
     left() : Robot {
-        return new Robot(this.x, this.y, directionLeft(this.direction));
+        if(this.isPlaced()) {
+            return this.copy(this._x, this._y, directionLeft(this._direction));
+        }
+        // Not placed, ignore.
+        return this;
     }
 
     right() : Robot {
-        return new Robot(this.x, this.y, directionRight(this.direction));
+        if(this.isPlaced()) {
+            return this.copy(this._x, this._y, directionRight(this._direction));
+        }
+        // Not placed, ignore.
+        return this;
+    }
+
+    private isPlaced() : boolean {
+        return this._x !== undefined && this._y !== undefined && this.direction !== undefined;
+    }
+
+    private isValid(x : number, y : number, direction : RobotDirection) {
+        return x !== undefined && x >= 0 && x <= this.maxY && y !== undefined && y >= 0 && y <= this.maxY && direction !== undefined
+    }
+
+    private copy(x : number, y : number, direction : RobotDirection) : Robot {
+        const robot = new Robot(this.maxX, this.maxY);
+        robot._x = x;
+        robot._y = y;
+        robot._direction = direction;
+        return robot
     }
 }
